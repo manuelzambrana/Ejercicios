ccf37b9a1a5044e1511897c1d218c766e8423c8e
Manuel Zambrana Jiménez <zambrana2613@gmail.com>
2017-10-13 08:41:03 +0200

    Ejercicios tema 3

---
+++ Ejercicios/tema3/Ejercicio3.java
@@ -0,0 +1,23 @@
+/**
+ * Conversor
+ *
+ * @author Manuel Zambrana Jimenez
+ */
+
+public class Ejercicio3 {
+  public static void main(String[] args) {
+    String linea;
+    System.out.print("Introduce una cantidad en pesetas para saber cuantos € son ");
+    int primerNumero = Integer.parseInt(System.console().readLine());
+    double euros=(primerNumero / 166.386);
+    System.out.print(primerNumero+" pesetas " +" son "+euros+" euros");
+    
+    
+    
+    
+    
+    
+    
+   
+  }
+}
