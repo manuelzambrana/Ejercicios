d0345666fa35a28c3739a86316327af757848e83
Manuel Zambrana Jiménez <zambrana2613@gmail.com>
2017-10-19 22:09:03 +0200

    añadiendo ejercicios tema 4

---
+++ Ejercicios/tema4/Ejercicio17.java
@@ -0,0 +1,26 @@
+/**
+ * ultima cifra
+ *
+ * @author Manuel Zambrana Jimenez
+ */
+
+public class Ejercicio17 {
+  public static void main(String[] args) {
+    System.out.println("Introduce un numero entero para saber su ultima cifra");
+    int a = Integer.parseInt(System.console() .readLine());
+    a=a%10;
+    System.out.println(a%10);
+    
+    
+    
+      
+      
+      
+    
+    
+  
+       
+    
+   
+  }
+}
