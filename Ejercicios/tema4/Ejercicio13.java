5a26441f4151c2ee0a40403f3d5b2bfb3c550818
Manuel Zambrana Jiménez <zambrana2613@gmail.com>
2017-10-19 18:34:09 +0200

    añadiendo ejercicios tema 4

---
+++ Ejercicios/tema4/Ejercicio13.java
@@ -0,0 +1,49 @@
+/**
+ * horas
+ *
+ * @author Manuel Zambrana Jimenez
+ */
+
+public class Ejercicio13 {
+  public static void main(String[] args) {
+    int aux; 
+    System.out.println("Introduce una primer numero");
+    int a = Integer.parseInt(System.console() .readLine());
+    
+    System.out.println("Introduce un segundo numero");
+    int b = Integer.parseInt(System.console() .readLine());
+    
+    
+    System.out.println("Introduce un tercer numero");
+    int c = Integer.parseInt(System.console() .readLine());
+    
+     if (a > b) {
+      aux = a;
+      a = b;
+      b = aux;
+    }
+    
+    
+    if (b > c) {
+      aux = b;
+      b = c;
+      c = aux;
+    }
+    
+    
+    if (a > b) {
+      aux = a;
+      a = b;
+      b = aux;
+    }
+        
+    System.out.println("Los números introducidos ordenados de menor a mayor son " + a + ", " + b + " y " + c + "."); 
+    
+    
+    
+  
+       
+    
+   
+  }
+}
